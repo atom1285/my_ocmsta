5fc7a779e39f7c897b33421f87b4b9ee6f6b722b
Atom1285 <ivan.polak25@gmail.com>
2022-10-25 19:04:46 +0000

    modified create scripts

--- scripts/create_subdomain.sh
+++ scripts/create_subdomain.sh
@@ -2,7 +2,15 @@
 
 set -e
 
-USER=$1
+if [ "$1" == "--preset" ]; then
+    USER=$2;
+else
+    echo "Enter the username for this subdomain";
+    read USER;
+fi
+
+echo "Creating a subdomain for '$USER' ";
+exit
 
 # rm /etc/apache2/sites-available/$USER.wezeo.fail.conf  
 USER=$USER envsubst < conf_templates/site.conf.tmpl > /etc/apache2/sites-available/$USER.wezeo.fail.conf
--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -2,11 +2,44 @@
 
 set -e # sets the scripts as non interactive
 
-USER=$1
-PASSWD=$2
-PASSWD_CONFIRM=$3
+while [ True ]; do
+if [ "$1" = "--preset" ]; then
+    PRESET=true
+    shift 1
+elif [ "$1" = "--create_subdomain" ]; then
+    CREATE_SUBDOMAIN=true
+    shift 1
+else
+    break
+fi
+done
 
-if [ $PASSWD != $PASSWD_CONFIRM ]; then
+if [ "$PRESET" = true ]; then
+    USER=$1;
+    PASSWORD=$2;
+    PASSWORD_CONFIRM=$3;
+
+    echo $USER;
+    echo $PASSWORD;
+    echo $PASSWORD_CONFIRM;
+else
+    echo "Please enter a username for the new user";
+    read USER;
+
+    echo "Please enter a password for the new user";
+
+    stty -echo;
+    read PASSWORD;
+    stty echo;
+
+    echo "Please enter the password again to confirm it";
+
+    stty -echo;
+    read PASSWORD_CONFIRM;
+    stty echo;
+fi
+
+if [ $PASSWORD != $PASSWORD_CONFIRM ]; then
     echo "Passwords do not match"
     exit 1
 fi
@@ -24,3 +57,7 @@ sudo sudo usermod -d /var/www/$USER.wezeo.fail $USER
 sudo chown -R $USER:subdomain_users /var/www/$USER.wezeo.fail
 sudo chmod -R 775 /var/www/$USER.wezeo.fail
 sudo chmod -R ugo+rw /var/www/$USER.wezeo.fail
+
+if [ "$CREATE_SUBDOMAIN" = true ]; then
+    /bin/bash create_subdomain.sh --preset $USER;
+fi
